aa896b4cd994a90102b1547805afa5573b667528
Austin Morrow <morrow.austin5@gmail.com>
2023-06-30 12:24:44 -0600

    under construction

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <Header />
     <Home />
-    <Work />
-    <Services />
   </div>
 </template>
 
 <script>
-import Header from "../src/layout/Header.vue";
 import Home from "../src/views/Home.vue";
-import Work from "../src/views/Work.vue";
-import Services from "../src/views/Services.vue";
 
 
 export default {
   name: "App",
   components: {
-    Header, Home, Work, Services
+   Home
   },
 };
 </script>
