9e74cc4aa2cc68ecba28cad18cf7fa9daa202f55
Austin Morrow <morrow.austin5@gmail.com>
2023-07-01 17:13:41 -0600

    Cleaned up some old images, started under construction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="big-square-image">
-    <h1 style="color: white">Some inspirational text</h1>
-    <router-link to="/contact">CONTACT FORM</router-link>
+  <div class="logo-container">
+    <img class="logo" src="../assets/images/dark_mode_logo.png" alt="Logo">
+    <div class="text-container">
+      <h2 class="text">New Website</h2>
+    </div>
+    
   </div>
 </template>
 
 <style scoped>
-.big-square-image {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.logo-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
   height: 100vh;
-  padding: 5px;
-  box-sizing: border-box;
   background-color: #2b2b2b;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; 
+  align-items: center;
 }
 
+.logo {
+  max-width: 450px; 
+  display: block;
+  margin-bottom: 10px;
+}
 
+.text-container {
+  text-align: center;
+}
+
+.text {
+  padding-top: 50px;
+  color: #fff;
+  font-size: 50px;
+}
 </style>
+
