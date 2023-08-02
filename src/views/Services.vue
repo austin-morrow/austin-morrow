7f7d4c406a515bbbde4713ad0c814901c55b99e8
Austin Morrow <morrow.austin5@gmail.com>
2023-08-02 08:27:33 -0600

    Getting services component set up

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,28 +1,76 @@
-<template class="container">
-  <div class="row">
-    <div class="col" style="color: white">
-      <h2 class="services">Design</h2>
-      <div style="padding: 20px;">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita pariatur enim ex consectetur dicta quidem quis doloribus quaerat, adipisci quisquam illo a quia accusantium!</p>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="service-content">
+          <div class="icon-box design-icon"></div>
+          <h2 class="services">Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita pariatur enim ex consectetur dicta quidem quis doloribus quaerat, adipisci quisquam illo a quia accusantium!</p>
+        </div>
       </div>
-    </div>
-    <div class="col" style="color: white">
-      <h2 class="services">Develop</h2>
-      <div style="padding: 20px;">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi officiis totam repudiandae, vitae in id excepturi alias quis facere saepe qui voluptatem, et cumque.</p>
+      <div class="col">
+        <div class="service-content">
+          <div class="icon-box develop-icon"></div>
+          <h2 class="services">Develop</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi officiis totam repudiandae, vitae in id excepturi alias quis facere saepe qui voluptatem, et cumque.</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+// Add any JavaScript logic here if needed
+</script>
 
-<style scoped>
-.services {
-  padding-left: 100px;
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden; /* Hide horizontal scrollbar if present */
+  background-color: #2b2b2b; /* Set background color for the entire page */
 }
 
 .container {
-  background-color: #2b2b2b;
+  padding-top: 100px;
+}
+
+.row {
+  display: flex;
+  justify-content: space-around;
+}
+
+.col {
+  color: white;
+  text-align: left;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.service-content {
+  text-align: left;
+}
+
+.icon-box {
+  width: 80px;
+  height: 80px;
+  background-color: #ffffff;
+  border-radius: 10px; /* Use a border radius to create square corners */
+  margin-bottom: 15px;
+}
+
+.design-icon {
+  /* Add any specific styles for the design icon here */
+}
+
+.develop-icon {
+  /* Add any specific styles for the develop icon here */
+}
+
+.services {
+  padding-left: 0;
+ 
 }
 </style>
