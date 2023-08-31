76b4764349f5309310b4ce548246933a40378870
Austin Morrow <austinmorrowdev@gmail.com>
2023-08-30 21:43:21 -0600

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
+import '../src/input.css'
 
 Vue.config.productionTip = false
 
