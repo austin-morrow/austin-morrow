f5f7eb27e0f1239edf76f5161017cd505f771549
Austin Morrow <austinmorrowdev@gmail.com>
2023-09-14 18:24:17 -0600

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Work from './pages/Work';
 import Services from './pages/Services';
 import Process from './pages/Process';
 import About from './pages/About';
-import Contact from './pages/Contact'
+import Contact from './pages/Contact';
+import Pricing from './pages/Pricing';
 
 function App() {
   return (
-    <>
-   <Header />
-   <Work />
-   <Services />
-   <Process />
-   <About />
-   <Contact />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<>
+          <Header />
+          <Work />
+          <Services />
+          <Process />
+          <About />
+          <Contact />
+        </>} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
