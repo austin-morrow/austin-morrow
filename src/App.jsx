500c54900d67e345d98b9bd8ddaaa51a1ef08b72
Austin Morrow <austinmorrowdev@gmail.com>
2023-10-05 07:55:41 -0600

    WIP set routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Pricing from './pages/Pricing';
+import Works from './pages/Works';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import About from './pages/About';
 import ScrollToTopButton from './components/ScrollToTopButton';
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
           <Home />
           <ScrollToTopButton />
         </>} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Works.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Works() {
+  return (
+    <div className="bg-primary-black py-24 sm:py-32">
+      <div className="text-white">
+      Works Page
+      </div>
+    </div>
+  );
+}
+
