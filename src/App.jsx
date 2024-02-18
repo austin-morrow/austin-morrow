6e3fecbcc3d970888c15d0e41576abd59c3d74e4
Austin Morrow <austinmorrowdev@gmail.com>
2024-02-18 10:16:13 -0700

    Fix footer and router going to middle of page

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Works from './pages/Works';
 import Footer from './components/Footer';
-
-
+import ScrollToTop from './components/ScrollToTop'; // Import ScrollToTop component
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="works" element={<Works />} />
-    </Routes>
-<Footer />
-  </BrowserRouter>
-
+      <Header />
+      <ScrollToTop /> {/* Include the ScrollToTop component */}
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="works" element={<Works />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
