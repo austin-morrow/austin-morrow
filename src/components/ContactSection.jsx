b63b557795ecfbed850b50a2d66b075984cf2a26
Austin Morrow <austinmorrowdev@gmail.com>
2024-12-26 21:00:44 -0700

    WIP

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,36 +1,66 @@
-import { Button } from '@/components/Button'
+'use client'
+
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
-import { Offices } from '@/components/Offices'
+import { useEffect, useRef } from 'react'
 
 export function ContactSection() {
+  const iframeRef = useRef(null)
+
+  useEffect(() => {
+    const moxieFrame = iframeRef.current
+    if (moxieFrame) {
+      moxieFrame.src =
+        'https://hello.withmoxie.com/01/austin-morrow1/discovery-form---new-website?inFrame=true&sourceUrl=' +
+        encodeURIComponent(window.location.href)
+    }
+
+    const handleResize = (event) => {
+      if (
+        event.origin === 'https://hello.withmoxie.com' &&
+        event.data &&
+        event.data.startsWith('height:')
+      ) {
+        const height = event.data.split(':')[1]
+        if (moxieFrame) {
+          moxieFrame.style.height = `${height}px`
+        }
+      }
+
+      if (
+        event.origin === 'https://hello.withmoxie.com' &&
+        event.data &&
+        event.data.startsWith('[Redirect]')
+      ) {
+        const url = event.data.slice(10)
+        window.location = url
+      }
+    }
+
+    window.addEventListener('message', handleResize, false)
+
+    return () => {
+      window.removeEventListener('message', handleResize)
+    }
+  }, [])
+
   return (
     <section name="contact">
-    <Container className="mt-24 sm:mt-32 lg:mt-40">
-      <FadeIn className="-mx-6 rounded-4xl bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12">
-        <div className="mx-auto max-w-4xl">
-          <div className="max-w-xl">
-            <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
-              Tell us about your project
-            </h2>
-            <div className="mt-6 flex">
-              <Button href="/contact" invert>
-                Say Hej
-              </Button>
-            </div>
-            <div className="mt-10 border-t border-white/10 pt-10">
-              <h3 className="font-display text-base font-semibold text-white">
-                Our offices
-              </h3>
-              <Offices
-                invert
-                className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2"
-              />
+      <Container className="mt-24 sm:mt-32 lg:mt-40">
+        <FadeIn className="-mx-6 rounded-4xl bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12">
+          <div className="mx-auto max-w-4xl">
+            <div className="min-h-[500px] w-full">
+              <iframe
+                ref={iframeRef}
+                id="moxie-discovery-form---new-website"
+                allowTransparency="true"
+                className="m-0 w-full border-0 p-0"
+                style={{ height: '500px' }} // Initial height
+              ></iframe>
             </div>
           </div>
-        </div>
-      </FadeIn>
-    </Container>
+        </FadeIn>
+      </Container>
     </section>
   )
 }
